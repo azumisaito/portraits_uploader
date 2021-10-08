ba7f90aff1e4beb3a6658b8a800b7355fb23145f
sazumy <49363119+sazumy@users.noreply.github.com>
2021-10-08 17:29:21 +0900

    articleモデルを作る

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_one_attached :portrait
+  has_many :articles
 end
---
+++ db/migrate/20211008082401_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
